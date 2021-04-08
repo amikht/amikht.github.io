1dc04865d04a737b75ab5a717359d522fefeba2f
Aaron Thompson <amt4049@rit.edu>
2021-04-08 09:57:57 -0400

    Adds profile link icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { Navbar, Container, Row, Col, NavItem, NavLink, NavbarBrand, Nav } from 'reactstrap';
 
 export default function Home() {
@@ -10,7 +11,52 @@ export default function Home() {
         
         <Row className="row" style={{minHeight: "100%", margin:0}}>
             <Col className="landing-col"> {/* Name */}
+                <div>
                 <h1>Aaron<br />Thompson</h1>
+                <Nav>
+                    <NavItem> {/** LinkedIn Icon */}
+                        <NavLink href="https://www.linkedin.com/in/aaron-t-software/">
+                            <Image
+                                src="/LinkedIn.png"
+                                alt="Linked In"
+                                width={48}
+                                height={48}
+
+                            />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem> {/** Github Icon */}
+                        <NavLink href="https://github.com/amikht">
+                            <Image
+                                src="/Github.png"
+                                alt="Github"
+                                width={48}
+                                height={48}
+                            />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem> {/** LinkedIn Icon */}
+                        <NavLink href="https://www.instagram.com/ohsrryitsjustme/" >
+                            <Image
+                                src="/Instagram.png"
+                                alt="Instagram"
+                                width={48}
+                                height={48}
+                            />
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="amt4049@rit.edu" >
+                            <Image
+                                src="/Mail.png"
+                                alt="Email"
+                                width={48}
+                                height={48}
+                            />
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                </div>
             </Col>
             <Col className="landing-col"> {/* Planet */}
                 <div>
